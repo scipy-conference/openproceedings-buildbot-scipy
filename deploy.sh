1ebaf625d71b05a700506e0732af4a12bcf560b4
Andrea Zonca <code@andreazonca.com>
2014-03-09 21:36:04 -0700

    Build PR in submission/ folder
    * get PR number from travis-ci environment
    * create folder submission/PRNUMBER
    * build all of the proceedings there
    * push to website
    
    new commits on the PR should trigger this and update the website,
    needs testing.
    Issue #1.

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,25 @@ TARGET_REPO=$GITHUB_ORGANIZATION/$GITHUB_ORGANIZATION.github.io
 TARGET_FOLDER=$1
 PELICAN_OUTPUT_FOLDER=output
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting to deploy to Github Pages\n"
-    if [ "$TRAVIS" == "true" ]; then
-        git config --global user.email "scipy-proceedings-bot@andreazonca.com"
-        git config --global user.name "scipy-proceedings-bot"
-    fi
-    #using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO.git built_website &> /dev/null
-    #go into directory and copy data we're interested in to that directory
-    cd built_website/$TARGET_FOLDER
-    rsync -rv --exclude=.git  $TRAVIS_BUILD_DIR/../buildbot/$PELICAN_OUTPUT_FOLDER/* .
-    #add, commit and push files
-    git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
-    git push -fq origin $BRANCH &> /dev/null
-    echo -e "Deploy completed\n"
+if [ "$TRAVIS" == "true" ]; then
+    git config --global user.email "scipy-proceedings-bot@andreazonca.com"
+    git config --global user.name "scipy-proceedings-bot"
 fi
+echo -e "Starting to deploy to Github Pages\n"
+COMMIT_MESSAGE="Travis build $TRAVIS_BUILD_NUMBER"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    TARGET_FOLDER=$TARGET_FOLDER/submission/$TRAVIS_PULL_REQUEST
+    COMMIT_MESSAGE="$COMMIT_MESSAGE, pull request $TRAVIS_PULL_REQUEST"
+fi
+
+#using token clone gh-pages branch
+git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO.git built_website &> /dev/null
+#go into directory and copy data we're interested in to that directory
+mkdir -p built_website/$TARGET_FOLDER
+cd built_website/$TARGET_FOLDER
+rsync -rv --exclude=.git  $TRAVIS_BUILD_DIR/../buildbot/$PELICAN_OUTPUT_FOLDER/* .
+#add, commit and push files
+git add -f .
+git commit -m $COMMIT_MESSAGE
+git push -fq origin $BRANCH &> /dev/null
+echo -e "Deploy completed\n"
